QubesOS/qubes-continuous-integration

commit fdfd5ca7dae3b59b84ed9b643fcdc4581c2d90e5

(re-)enable devel builds for Arch
QubesOS/qubes-issues#10478

dockerfiles/vm-archlinux.Dockerfile
@@ -7,6 +7,9 @@ RUN pacman-key --lsign "$(gpg --with-colons --show-key /tmp/qubes-repo-archlinux
 RUN wget -O /tmp/qubes-repo-archlinux-key.asc https://raw.githubusercontent.com/QubesOS/qubes-secpack/main/keys/template-keys/qubes-release-4.3-archlinux.asc
 RUN pacman-key --add - < /tmp/qubes-repo-archlinux-key.asc
 RUN pacman-key --lsign "$(gpg --with-colons --show-key /tmp/qubes-repo-archlinux-key.asc -| grep ^fpr: | cut -d : -f 10)"
+RUN wget -O /tmp/qubes-repo-archlinux-key https://raw.githubusercontent.com/QubesOS/qubes-builderv2/main/qubesbuilder/plugins/chroot_archlinux/keys/qubes-repo-archlinux-key-rdevel.asc
+RUN pacman-key --add - < /tmp/qubes-repo-archlinux-key.asc
+RUN pacman-key --lsign "$(gpg --with-colons --show-key /tmp/qubes-repo-archlinux-key.asc -| grep ^fpr: | cut -d : -f 10)"
 
 RUN printf '\
 [qubes-r4.2-current-testing]\n\

.gitlab-ci.yml
@@ -5,7 +5,7 @@ include:
   - '/devel/gitlab-host-vm-openqa.yml'
   - '/devel/gitlab-vm-fedora-build.yml'
   - '/devel/gitlab-vm-debian-build.yml'
-#  - '/devel/gitlab-vm-archlinux-build.yml'
+  - '/devel/gitlab-vm-archlinux-build.yml'
 
 workflow:
   rules:

devel/gitlab-vm-archlinux.yml
@@ -1,3 +1,3 @@
 include: []
-#  - '/devel/gitlab-vm-archlinux-build.yml'
-#  - '/devel/gitlab-vm-archlinux-install.yml'
+  - '/devel/gitlab-vm-archlinux-build.yml'
+  - '/devel/gitlab-vm-archlinux-install.yml'

scripts/gitlab-installv2
@@ -51,8 +51,9 @@ def main(release: str, qubes_distribution: QubesDistribution, repository: Path):
         )
     elif qubes_distribution.fullname == "archlinux":
         pkgext = "pkg.tar.zst"
+        version_str = "devel" if release == "devel" else f"r{release}"
         run_cmd = (
-            f"sed -i -e 's/4\\.2/{release}/g ' /etc/pacman.conf && "
+            f"sed -i -e 's/r4\\.2/{version_str}/g ' /etc/pacman.conf && "
             "pacman -Sy --noprogressbar  && pacman -U --noconfirm --noprogressbar"
         )
     else:

commit 6df97c0cc4cd2c0bbe7d1fe1b81a6983ec8dc928

archlinux: fix fetching devel key
Fixes: fdfd5ca "(re-)enable devel builds for Arch"

dockerfiles/vm-archlinux.Dockerfile
@@ -7,7 +7,7 @@ RUN pacman-key --lsign "$(gpg --with-colons --show-key /tmp/qubes-repo-archlinux
 RUN wget -O /tmp/qubes-repo-archlinux-key.asc https://raw.githubusercontent.com/QubesOS/qubes-secpack/main/keys/template-keys/qubes-release-4.3-archlinux.asc
 RUN pacman-key --add - < /tmp/qubes-repo-archlinux-key.asc
 RUN pacman-key --lsign "$(gpg --with-colons --show-key /tmp/qubes-repo-archlinux-key.asc -| grep ^fpr: | cut -d : -f 10)"
-RUN wget -O /tmp/qubes-repo-archlinux-key https://raw.githubusercontent.com/QubesOS/qubes-builderv2/main/qubesbuilder/plugins/chroot_archlinux/keys/qubes-repo-archlinux-key-rdevel.asc
+RUN wget -O /tmp/qubes-repo-archlinux-key.asc https://raw.githubusercontent.com/QubesOS/qubes-builderv2/main/qubesbuilder/plugins/chroot_archlinux/keys/qubes-repo-archlinux-key-rdevel.asc
 RUN pacman-key --add - < /tmp/qubes-repo-archlinux-key.asc
 RUN pacman-key --lsign "$(gpg --with-colons --show-key /tmp/qubes-repo-archlinux-key.asc -| grep ^fpr: | cut -d : -f 10)"
 